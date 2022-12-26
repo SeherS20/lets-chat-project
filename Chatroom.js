10ed0013a66f5e5918061e6e975ed03f6b5947cf
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2022-12-26 12:58:51 -0500

    Add files via upload

---
+++ Chatroom.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0CcnXFY78B2GxzppuVM3FWVIPW8DaEIE",
+    authDomain: "letschat-app-b5356.firebaseapp.com",
+    databaseURL: "https://letschat-app-b5356-default-rtdb.firebaseio.com",
+    projectId: "letschat-app-b5356",
+    storageBucket: "letschat-app-b5356.appspot.com",
+    messagingSenderId: "960281001475",
+    appId: "1:960281001475:web:a3687ea3352c0e71c8d836",
+    measurementId: "G-S54SZT4JB1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
